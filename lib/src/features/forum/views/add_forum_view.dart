5b9aa54d433b3ce0005c92cae78c02f9321e96db
GMO <gusanum5@gmail.com>
2022-08-02 00:40:00 +0100

    image-picker

--- lib/src/features/forum/views/add_forum_view.dart
+++ lib/src/features/forum/views/add_forum_view.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:ninja_connect/src/core/constants/colors.dart';
 import 'package:ninja_connect/src/core/constants/dimensions.dart';
 import 'package:ninja_connect/src/core/utilities/validation_extension.dart';
 import 'package:ninja_connect/src/widgets/app_buttons.dart';
@@ -13,15 +16,27 @@ class AddForumView extends StatefulWidget {
 }
 
 class _AddForumViewState extends State<AddForumView> {
+  final ImagePicker _picker = ImagePicker();
+  XFile? imageFile;
   final _formKey = GlobalKey<FormState>();
   final forum = TextEditingController();
   final summary = TextEditingController();
   final about = TextEditingController();
+
+  void filePicker() async {
+    final selectImage = await _picker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      imageFile = selectImage;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
+    final colorScheme = Theme.of(context).colorScheme;
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: AppColors.light,
         title: Text(
           'Add New Forum',
           style: textTheme.headline6,
@@ -39,6 +54,43 @@ class _AddForumViewState extends State<AddForumView> {
                 labelText: 'Forum Name',
                 validator: (value) =>
                     context.validateFieldNotEmpty(value, context),
+              ),
+              const Spacing.bigHeight(),
+              Container(
+                decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.black54,
+                    ),
+                    borderRadius: const BorderRadius.all(Radius.circular(10))),
+                height: 200,
+                child: imageFile == null
+                    ? GestureDetector(
+                        onTap: () {
+                          filePicker();
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.file_upload_sharp,
+                              color: colorScheme.onBackground,
+                            ),
+                            const Spacing.smallWidth(),
+                            Text(
+                              'Pick Image',
+                              style: textTheme.headline6?.copyWith(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    : Image.file(
+                        File(imageFile!.path),
+                        fit: BoxFit.cover,
+                        width: 370,
+                      ),
               ),
               const Spacing.bigHeight(),
               AppTextField(
---
+++ lib/src/repositories/add_forum_repository.dart
@@ -0,0 +1,7 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class AddForumRepository {}
+
+final addForumRepository = Provider((ref) {
+  return AddForumRepository();
+});
